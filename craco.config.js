e09e8072e101c358367e9672a7e995f3e1b7be0f
jihye <byjihye@hashsnap.net>
2021-01-06 17:12:48 +0900

    purgecss를 환경변수 production일 때만 실행하기

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,12 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
   style: {
     postcss: {
       plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
+        !isDev && require('@fullhuman/postcss-purgecss')({
           content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
         }),
       ],
